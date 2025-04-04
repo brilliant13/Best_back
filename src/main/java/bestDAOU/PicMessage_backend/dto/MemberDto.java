361bc8eeeaebc2e94e4b63addbe1f3934da533f3
Chamin Im <ckals413@naver.com>
2025-04-05 00:26:29 +0900

    :sparkles:   [#5] Member 필드 명세

--- src/main/java/bestDAOU/PicMessage_backend/dto/MemberDto.java
+++ src/main/java/bestDAOU/PicMessage_backend/dto/MemberDto.java
@@ -1,23 +1,30 @@
 package bestDAOU.PicMessage_backend.dto;
 
-import bestDAOU.PicMessage_backend.entity.Friends;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
+@Schema(description = "회원 정보 DTO")
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberDto {
+
+    @Schema(description = "회원 ID", example = "1")
     private Long id;
+
+    @Schema(description = "회원 이름", example = "김찰스", required = true)
     private String name;
+
+    @Schema(description = "비밀번호", example = "password123", required = true)
     private String password;
+
+    @Schema(description = "이메일", example = "user@example.com", required = true)
     private String email;
+
+    @Schema(description = "전화번호", example = "01012341234", required = true)
     private String phone;
 }
