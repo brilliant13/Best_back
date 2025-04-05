0f679e078cec3ca344fb29dcf7b33edf86e2cb63
Chamin Im <ckals413@naver.com>
2025-04-05 19:22:55 +0900

    :sparkles:   [#5] 원래대로 (예찬)

--- src/main/java/bestDAOU/PicMessage_backend/mapper/FriendsMapper.java
+++ src/main/java/bestDAOU/PicMessage_backend/mapper/FriendsMapper.java
@@ -24,19 +24,20 @@ public class FriendsMapper {
     }
 
     // FriendsDto -> Friends 엔티티로 매핑
+    // FriendsDto -> Friends 엔티티로 매핑
     public static Friends mapToFriends(FriendsDto friendsDto, Member member) {
-        Friends friends = new Friends();
-        friends.setId(friendsDto.getId());
-        friends.setFriendName(friendsDto.getFriendName());
-        friends.setFriendPhone(friendsDto.getFriendPhone());
-        friends.setFriendEmail(friendsDto.getFriendEmail());
-        friends.setFeatures(friendsDto.getFeatures());
-        friends.setMemos(friendsDto.getMemos());
-        friends.setTones(friendsDto.getTones());
-        friends.setTones_prompt(friendsDto.getTones_prompt());
-        friends.setMember(member); // Member 엔티티를 직접 설정
-        friends.setRelationType(friendsDto.getRelationType());
-        friends.setGroupName(friendsDto.getGroupName());
-        return friends;
+        return new Friends(
+                friendsDto.getId(),
+                friendsDto.getFriendName(),
+                friendsDto.getFriendPhone(),
+                friendsDto.getFriendEmail(),
+                friendsDto.getFeatures(),
+                friendsDto.getMemos(),
+                friendsDto.getTones(),
+                friendsDto.getTones_prompt(),
+                member, // Member 엔티티를 직접 설정
+                friendsDto.getRelationType(),
+                friendsDto.getGroupName()
+        );
     }
 }
