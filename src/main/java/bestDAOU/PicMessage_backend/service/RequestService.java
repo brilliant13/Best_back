b4e60599318c828b18d7b74e1f02198008f117d4
moonkwon12 <ok63477@gmail.com>
2025-05-03 15:44:32 +0900

    뿌리오 문자 전송 계정이름 수정

--- src/main/java/bestDAOU/PicMessage_backend/service/RequestService.java
+++ src/main/java/bestDAOU/PicMessage_backend/service/RequestService.java
@@ -21,7 +21,7 @@ public class RequestService {
     private String PpurioAiApiKey;
 
     private static final Integer TIME_OUT = 5000;
-    private static final String PPURIO_ACCOUNT = "jjww6367";
+    private static final String PPURIO_ACCOUNT = "jjww6368";
     private static final String FROM = "01092014486";
     private static final String URI = "https://message.ppurio.com";
 
