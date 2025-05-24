bc648d50c0da13f45a8cb5713709b2d673055266
Woong Jung <kaka366@naver.com>
2025-05-24 15:07:43 +0900

    WebConfig수정, localhost & ip주소 입력 중복허용

--- src/main/java/bestDAOU/PicMessage_backend/config/WebConfig.java
+++ src/main/java/bestDAOU/PicMessage_backend/config/WebConfig.java
@@ -13,8 +13,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOriginPatterns("http://localhost:3000") // React 앱이 실행되는 포트
-                        .allowedOriginPatterns("http://223.194.137.227:3000") // React 앱이 실행되는 포트
+//                        .allowedOriginPatterns("http://localhost:3000") // React 앱이 실행되는 포트
+//                        .allowedOriginPatterns("http://223.194.154.236:3000") // React 앱이 실행되는 포트
+                        .allowedOriginPatterns(
+                                "http://localhost:3000",
+                                "http://223.194.154.236:3000"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 자격 증명 허용
